be2693ffd3e742fed6991707008be7e4d2df6a1b
MrNullus <gustavojs417@gmail.com>
2024-10-08 09:38:21 -0300

    INIT

---
+++ sql/basic/weather-observartion-station-7/solution.sql
@@ -0,0 +1,9 @@
+SELECT 
+    DISTINCT  CITY
+FROM
+    STATION
+WHERE 
+    (LAT_N IS NOT NULL)  AND
+    (LONG_W IS NOT NULL) AND 
+    LOWER(SUBSTRING(CITY, -1)) IN ('a', 'e', 'i', 'o', 'u')
+;
---
+++ sql/basic/weather-observartion-station-8/solution.sql
@@ -0,0 +1,11 @@
+SELECT 
+    DISTINCT  CITY
+FROM
+    STATION
+WHERE 
+    (LAT_N IS NOT NULL)  AND
+    (LONG_W IS NOT NULL) AND 
+    LOWER(SUBSTRING(CITY, -1)) IN ('a', 'e', 'i', 'o', 'u') AND
+    LOWER(SUBSTRING(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u')
+ORDER BY CITY
+;
